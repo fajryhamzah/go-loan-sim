e7e3b8c5fe329097e071e52d79e7d70577144041
Fajry Hamzah <fajryhamzah@gmail.com>
2025-11-15 08:26:13 +0700

    feat: adjust main data struct

--- types/loan.go
+++ types/loan.go
@@ -3,20 +3,23 @@ package types
 import "time"
 
 type LoanPaymentSchedule struct {
-	Amount     int
-	PaidAmount int
-	Status     string
-	DueDate    time.Time
+	LoanPaymentScheduleID string
+	Amount                int
+	PaidAmount            int
+	Status                string
+	DueDate               time.Time
 }
 
 type Loan struct {
+	LoanID              string
 	PrincipalLoanAmount int
 	TotalLoanAmount     int
 	PaidAmount          int
-	PendingAmount       int
+	WeeklyPaymentAmount int
 	MissPayment         int
 	Interest            float32
+	Status              string
 	LoanDate            time.Time
 	LoadEndDate         time.Time
-	LoanPaymentSchedule []LoanPaymentSchedule
+	LoanPaymentSchedule []*LoanPaymentSchedule
 }
--- types/user.go
+++ types/user.go
@@ -1,6 +1,7 @@
 package types
 
 type User struct {
+	UserID      string
 	Name        string
 	LoanActive  *Loan
 	LoanHistory []*Loan
